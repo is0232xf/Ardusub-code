84651a7206f0bb2a12a616ce2dd1443baf19ef22
fujii <is0232xf@gmail.com>
2019-10-18 00:48:55 +0900

    [fix] fix mission code and move code

---
+++ TestMission.py
@@ -0,0 +1,57 @@
+import time
+from ArduPilotDriver import ArduPilotDriver
+
+UAV = ArduPilotDriver()
+
+UAV.start()
+
+
+UAV.takeoff(5)
+
+print("")
+print("#################################################################")
+print("# Test move to coordinates")
+
+# print("")
+# print ">>Move to lat, lon (-35.3630, 149.165218)"
+# UAV.setAirSpeed(5)
+# UAV.nav_goto_gps_coordinates(-35.3630, 149.165218)
+# time.sleep(2)
+
+print("")
+print (">>Move to way point 1")
+UAV.setAirSpeed(0)
+UAV.nav_goto_gps_coordinates(34.982122, 135.963603)
+print("Succeeded")
+time.sleep(2)
+
+print("")
+print(">>Move to way point 2")
+UAV.setAirSpeed(0)
+UAV.nav_goto_gps_coordinates(34.982180, 135.963606)
+print("Succeeded")
+time.sleep(2)
+
+print("")
+print (">>Move to way point 3")
+UAV.setAirSpeed(0)
+UAV.nav_goto_gps_coordinates(34.982184, 135.963684)
+print("Succeeded")
+time.sleep(2)
+
+print("")
+print("#################################################################")
+print("# Return to Home")
+
+print("")
+print (">>Return")
+UAV.setAirSpeed(3)
+UAV.returnToHome()
+
+#print("")
+#print("#################################################################")
+#print("# Land")
+
+#UAV.land()
+
+UAV.exit()
